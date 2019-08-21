0b348525f78d5dc85b1a2ab4a793586534f30989
wangym <642688028@qq.com>
2019-08-21 11:23:31 +0800

    mater modified

--- core/src/main/java/com/gxa/p2p/common/domain/LoginInfo.java
+++ core/src/main/java/com/gxa/p2p/common/domain/LoginInfo.java
@@ -1,10 +1,13 @@
 package com.gxa.p2p.common.domain;
 
+import lombok.Data;
+
+@Data
 public class LoginInfo {
     public static final byte STATE_NORMAL = 0;
     private Long id;
 
-    private String username;
+    private String username ;
 
     private String password;
 
@@ -14,51 +17,5 @@ public class LoginInfo {
 
     private Boolean admin;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username == null ? null : username.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public Byte getState() {
-        return state;
-    }
-
-    public void setState(Byte state) {
-        this.state = state;
-    }
-
-    public Byte getUsertype() {
-        return usertype;
-    }
-
-    public void setUsertype(Byte usertype) {
-        this.usertype = usertype;
-    }
-
-    public Boolean getAdmin() {
-        return admin;
-    }
-
-    public void setAdmin(Boolean admin) {
-        this.admin = admin;
-    }
 }
